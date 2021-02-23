e143cbbbcf3985090bc90a4a10878076d17171ef
7KC-ANDROID <akshayraut50159832GMAIL.COM>
2021-02-24 03:34:00 +0530

    Initial

---
+++ app/src/main/java/com/example/webplat/database/AppDatabase.java
@@ -0,0 +1,12 @@
+package com.example.webplat.database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Recipe.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract RecipeDao recipeDao();
+
+
+}
---
+++ app/src/main/java/com/example/webplat/database/RecipeDao.java
@@ -0,0 +1,17 @@
+package com.example.webplat.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface RecipeDao {
+
+    @Query("SELECT * FROM Recipe")
+    List<Recipe> getAll();
+
+    @Insert
+    void insert(Recipe recipe);
+}
